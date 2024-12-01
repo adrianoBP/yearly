98def29b3db69d85f86645524a9c0d1e8426da10
adrianoBP <adriano.boccardo@gmail.com>
2024-12-01 14:59:04 +0000

    🐛 stop propagation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,7 +18,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     {
       provide: 'version',
-      useValue: '0.3.3',
+      useValue: '0.3.4',
     },
     {
       provide: 'mockData',
